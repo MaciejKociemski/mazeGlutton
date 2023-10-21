d4fdc97c5c4d7eca91803d34faec5029cc2a1d01
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-10-21 12:45:26 +0200

    add game and ghost and corrections

--- game.js
+++ game.js
@@ -1,27 +1,43 @@
+const gameOverElement = document.getElementById("game-over");
+const finalScoreElement = document.getElementById("final-score");
+const restartButton = document.getElementById("restart-button");
+
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
 const mazegluttonFrames = document.getElementById("animations");
-const ghostFrames = document.getElementById("ghost");
+const ghostFrames = document.getElementById("ghosts");
 
 let createRect = (x, y, width, height, color) => {
   canvasContext.fillStyle = color;
   canvasContext.fillRect(x, y, width, height);
 };
 
-let fps = 30;
-let oneBlockSize = 20;
-let wallColor = "#342DCA";
-let wallSpaceWidth = oneBlockSize / 1.5;
-let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
-let wallInnerColor = "black";
-
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
 const DIRECTION_LEFT = 2;
 const DIRECTION_BOTTOM = 1;
+let lives = 3;
+let ghostCount = 4;
+let ghostImageLocations = [
+  { x: 0, y: 0 },
+  { x: 176, y: 0 },
+  { x: 0, y: 121 },
+  { x: 176, y: 121 },
+];
 
-// let mazeglutton;
+// Game variables
+let fps = 30;
+let mazeglutton;
+let oneBlockSize = 20;
+let score = 0;
+let ghosts = [];
+let wallSpaceWidth = oneBlockSize / 1.6;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
 
+// we now create the map of the walls,
+// if 1 wall, if 0 not wall
+// 21 columns // 23 rows
 let map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
@@ -33,14 +49,14 @@ let map = [
   [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
   [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
-  [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1],
+  [1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1],
   [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
   [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
   [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
-  [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+  [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
   [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
   [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
   [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
@@ -48,37 +64,136 @@ let map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+let randomTargetsForGhosts = [
+  { x: 1 * oneBlockSize, y: 1 * oneBlockSize },
+  { x: 1 * oneBlockSize, y: (map.length - 2) * oneBlockSize },
+  { x: (map[0].length - 2) * oneBlockSize, y: oneBlockSize },
+  {
+    x: (map[0].length - 2) * oneBlockSize,
+    y: (map.length - 2) * oneBlockSize,
+  },
+];
+
+let createNewMazeglutton = () => {
+  mazeglutton = new Mazeglutton(
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize / 5
+  );
+};
+
 let gameLoop = () => {
-  update();
-  draw();
+  if (lives > 0) {
+    update();
+    draw();
+  } else {
+    endGame(); // End the game when lives reach 0
+  }
+};
+
+let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+let restartMazegluttonAndGhosts = () => {
+  createNewMazeglutton();
+  createGhosts();
+};
+
+let onGhostCollision = () => {
+  lives--;
+  if (lives === 0) {
+    endGame(); // End the game when lives reach 0
+  } else {
+    restartMazegluttonAndGhosts();
+  }
 };
 
 let update = () => {
-  //todo
-    mazeglutton.moveProcess()
+  mazeglutton.moveProcess();
+  mazeglutton.eat();
+  updateGhosts();
+  if (mazeglutton.checkGhostCollision(ghosts)) {
+    onGhostCollision();
+  }
+};
+
+let drawFoods = () => {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] == 2) {
+        createRect(
+          j * oneBlockSize + oneBlockSize / 3,
+          i * oneBlockSize + oneBlockSize / 3,
+          oneBlockSize / 3,
+          oneBlockSize / 3,
+          "#fff"
+        );
+      }
+    }
+  }
+};
+
+let drawRemainingLives = () => {
+  canvasContext.font = "20px Emulogic";
+  canvasContext.fillStyle = "white";
+  canvasContext.fillText("Lives: ", 220, oneBlockSize * (map.length + 1));
+
+  for (let i = 0; i < lives; i++) {
+    canvasContext.drawImage(
+      mazegluttonFrames,
+      2 * oneBlockSize,
+      0,
+      oneBlockSize,
+      oneBlockSize,
+      350 + i * oneBlockSize,
+      oneBlockSize * map.length + 2,
+      oneBlockSize,
+      oneBlockSize
+    );
+  }
+};
+
+let drawScore = () => {
+  canvasContext.font = "20px Emulogic";
+  canvasContext.fillStyle = "white";
+  canvasContext.fillText(
+    "       Score: " + score,
+    0,
+    oneBlockSize * (map.length + 1)
+  );
 };
 
 let draw = () => {
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
   createRect(0, 0, canvas.width, canvas.height, "black");
-    drawWalls();
-    mazeglutton.draw();
+  drawWalls();
+  drawFoods();
+  drawGhosts();
+  mazeglutton.draw();
+  drawScore();
+  drawRemainingLives();
 };
 
-
-
-let gameInterval = setInterval(gameLoop, 1000 / fps);
+const endGame = () => {
+  clearInterval(gameInterval); // Stop the game loop
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+   
+  // Display the game over message
+  gameOverElement.style.display = "block";
+  finalScoreElement.textContent = score;
+};
 
 let drawWalls = () => {
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[0].length; j++) {
-      if (map[i][j] === 1) {
-        //then it is a wall
+      if (map[i][j] == 1) {
         createRect(
           j * oneBlockSize,
           i * oneBlockSize,
           oneBlockSize,
           oneBlockSize,
-          wallColor
+          "#666"
         );
         if (j > 0 && map[i][j - 1] == 1) {
           createRect(
@@ -89,6 +204,7 @@ let drawWalls = () => {
             wallInnerColor
           );
         }
+
         if (j < map[0].length - 1 && map[i][j + 1] == 1) {
           createRect(
             j * oneBlockSize + wallOffset,
@@ -98,19 +214,21 @@ let drawWalls = () => {
             wallInnerColor
           );
         }
-        if (i > 0 && map[i - 1][j] == 1) {
+
+        if (i < map.length - 1 && map[i + 1][j] == 1) {
           createRect(
             j * oneBlockSize + wallOffset,
-            i * oneBlockSize,
+            i * oneBlockSize + wallOffset,
             wallSpaceWidth,
             wallSpaceWidth + wallOffset,
             wallInnerColor
           );
         }
-        if (i < map.length - 1 && map[i + 1][j] == 1) {
+
+        if (i > 0 && map[i - 1][j] == 1) {
           createRect(
             j * oneBlockSize + wallOffset,
-            i * oneBlockSize + wallOffset,
+            i * oneBlockSize,
             wallSpaceWidth,
             wallSpaceWidth + wallOffset,
             wallInnerColor
@@ -121,17 +239,66 @@ let drawWalls = () => {
   }
 };
 
-
-let createNewMazeglutton = () => {
-  mazeglutton = new Mazeglutton(
-    oneBlockSize,
-    oneBlockSize,
-    oneBlockSize,
-      oneBlockSize / 5,
-    map
-  );
+let createGhosts = () => {
+  ghosts = [];
+  for (let i = 0; i < ghostCount * 2; i++) {
+    let newGhost = new Ghost(
+      9 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      10 * oneBlockSize + (i % 2 == 0 ? 0 : 1) * oneBlockSize,
+      oneBlockSize,
+      oneBlockSize,
+      mazeglutton.speed / 2,
+      ghostImageLocations[i % 4].x,
+      ghostImageLocations[i % 4].y,
+      124,
+      116,
+      6 + i
+    );
+    ghosts.push(newGhost);
+  }
 };
 
-
 createNewMazeglutton();
-gameLoop();
+createGhosts();
+
+window.addEventListener("keydown", (event) => {
+  let k = event.keyCode;
+  setTimeout(() => {
+    if (k == 37 || k == 65) {
+      // left arrow or a
+      mazeglutton.nextDirection = DIRECTION_LEFT;
+    } else if (k == 38 || k == 87) {
+      // up arrow or w
+      mazeglutton.nextDirection = DIRECTION_UP;
+    } else if (k == 39 || k == 68) {
+      // right arrow or d
+      mazeglutton.nextDirection = DIRECTION_RIGHT;
+    } else if (k == 40 || k == 83) {
+      // bottom arrow or s
+      mazeglutton.nextDirection = DIRECTION_BOTTOM;
+    }
+  }, 1);
+});
+
+
+
+restartButton.addEventListener("click", () => {
+  // Reset the lives, score, and map
+  lives = 3;
+  score = 0;
+  // Reset the map or other elements if needed
+
+    
+   
+  // Hide the game over message
+  gameOverElement.style.display = "none";
+
+  // Create a new Mazeglutton and restart the ghosts
+    
+  createNewMazeglutton();
+  createGhosts();
+
+  // Start the game loop again
+    gameInterval = setInterval(gameLoop, 1000 / fps);
+    
+});
--- mazeglutton.js
+++ mazeglutton.js
@@ -1,138 +1,172 @@
 class Mazeglutton {
-  constructor(x, y, width, height, speed) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.speed = speed;
-    this.direction = DIRECTION_RIGHT;
-    this.currentFrame = 1;
-    this.frameCount = 7;
-    this.map = map;
-
-    setInterval(() => {
-      this.changeAnimation();
-    }, 100);
-  }
-
-  moveProcess() {
-    this.changeDirectionIfPossible();
-    this.moveForwards();
-    if (this.checkCollision()) {
-      this.moveBackwards();
+    constructor(x, y, width, height, speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = 4;
+        this.nextDirection = 0;
+        this.frameCount = 7;
+        this.currentFrame = 1;
+        setInterval(() => {
+            this.changeAnimation();
+        }, 100);
     }
-  }
 
-  eat() {}
-
-  moveBackwards() {
-    switch (this.direction) {
-      case DIRECTION_RIGHT:
-        this.x -= this.speed;
-        break;
-      case DIRECTION_UP:
-        this.y += this.speed;
-        break;
-      case DIRECTION_LEFT:
-        this.x += this.speed;
-        break;
-      case DIRECTION_BOTTOM:
-        this.y -= this.speed;
-        break;
+    moveProcess() {
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if (this.checkCollisions()) {
+            this.moveBackwards();
+            return;
+        }
     }
-  }
-  moveForwards() {
-    switch (this.direction) {
-      case DIRECTION_RIGHT:
-        this.x += this.speed;
-        break;
-      case DIRECTION_UP:
-        this.y -= this.speed;
-        break;
-      case DIRECTION_LEFT:
-        this.x -= this.speed;
-        break;
-      case DIRECTION_BOTTOM:
-        this.y += this.speed;
-        break;
+
+    eat() {
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[0].length; j++) {
+                if (
+                    map[i][j] == 2 &&
+                    this.getMapX() == j &&
+                    this.getMapY() == i
+                ) {
+                    map[i][j] = 3;
+                    score++;
+                }
+            }
+        }
     }
-  }
 
-  checkCollision() {
-    if (
-      this.map[this.getMapY()][this.getMapX()] == 1 ||
-      this.map[this.getMapYRightSide()][this.getMapX()] == 1 ||
-      this.map[this.getMapY()][this.getMapXRightSide()] == 1 ||
-      this.map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
-    ) {
-      return true;
+    moveBackwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT: // Right
+                this.x -= this.speed;
+                break;
+            case DIRECTION_UP: // Up
+                this.y += this.speed;
+                break;
+            case DIRECTION_LEFT: // Left
+                this.x += this.speed;
+                break;
+            case DIRECTION_BOTTOM: // Bottom
+                this.y -= this.speed;
+                break;
+        }
     }
-    return false;
-  }
 
-  checkCollision() {
-    if (
-      map[this.getMapY()][this.getMapX()] == 1 ||
-      map[this.getMapYRightSide()][this.getMapX()] == 1 ||
-      map[this.getMapY()][this.getMapXRightSide()] == 1 ||
-      map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
-    ) {
-      return true;
+    moveForwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT: // Right
+                this.x += this.speed;
+                break;
+            case DIRECTION_UP: // Up
+                this.y -= this.speed;
+                break;
+            case DIRECTION_LEFT: // Left
+                this.x -= this.speed;
+                break;
+            case DIRECTION_BOTTOM: // Bottom
+                this.y += this.speed;
+                break;
+        }
     }
-    return false;
-  }
 
-  checkGhostCollision() {}
+    checkCollisions() {
+        let isCollided = false;
+        if (
+            map[parseInt(this.y / oneBlockSize)][
+                parseInt(this.x / oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize + 0.9999)][
+                parseInt(this.x / oneBlockSize)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize)][
+                parseInt(this.x / oneBlockSize + 0.9999)
+            ] == 1 ||
+            map[parseInt(this.y / oneBlockSize + 0.9999)][
+                parseInt(this.x / oneBlockSize + 0.9999)
+            ] == 1
+        ) {
+            isCollided = true;
+        }
+        return isCollided;
+    }
 
-  changeDirectionIfPossible() {}
+    checkGhostCollision(ghosts) {
+        for (let i = 0; i < ghosts.length; i++) {
+            let ghost = ghosts[i];
+            if (
+                ghost.getMapX() == this.getMapX() &&
+                ghost.getMapY() == this.getMapY()
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-  changeAnimation() {
-    this.currentFrame =
-      this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
-  }
+    changeDirectionIfPossible() {
+        if (this.direction == this.nextDirection) return;
+        let tempDirection = this.direction;
+        this.direction = this.nextDirection;
+        this.moveForwards();
+        if (this.checkCollisions()) {
+            this.moveBackwards();
+            this.direction = tempDirection;
+        } else {
+            this.moveBackwards();
+        }
+    }
 
-  draw() {
-    canvasContext.save();
-    canvasContext.translate(
-      this.x + oneBlockSize / 2,
-      this.y + oneBlockSize / 2
-    );
+    getMapX() {
+        let mapX = parseInt(this.x / oneBlockSize);
+        return mapX;
+    }
 
-    canvasContext.rotate((this.direction * 90 * Math.PI) / 180);
+    getMapY() {
+        let mapY = parseInt(this.y / oneBlockSize);
 
-    canvasContext.translate(
-      -this.x - oneBlockSize / 2,
-      -this.y - oneBlockSize / 2
-    );
+        return mapY;
+    }
 
-    canvasContext.drawImage(
-      mazegluttonFrames,
-      (this.currentFrame - 1) / oneBlockSize,
-      0,
-      oneBlockSize,
-      oneBlockSize,
-      this.x,
-      this.y,
-      this.width,
-      this.height
-    );
+    getMapXRightSide() {
+        let mapX = parseInt((this.x * 0.99 + oneBlockSize) / oneBlockSize);
+        return mapX;
+    }
 
-    canvasContext.restore();
-  }
+    getMapYRightSide() {
+        let mapY = parseInt((this.y * 0.99 + oneBlockSize) / oneBlockSize);
+        return mapY;
+    }
 
-  getMapX() {
-    return Math.floor(this.x / oneBlockSize); // Użyj Math.floor, aby zaokrąglić w dół
-  }
+    changeAnimation() {
+        this.currentFrame =
+            this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1;
+    }
 
-  getMapY() {
-    return Math.floor(this.y / oneBlockSize); // Użyj Math.floor, aby zaokrąglić w dół
-  }
-
-  getMapXRightSide() {
-    return Math.floor((this.x + this.width) / oneBlockSize); // Użyj Math.floor, aby zaokrąglić w dół
-  }
-
-  getMapYRightSide() {
-    return Math.floor((this.y + this.height) / oneBlockSize); // Użyj Math.floor, aby zaokrąglić w dół
-  }
+    draw() {
+        canvasContext.save();
+        canvasContext.translate(
+            this.x + oneBlockSize / 2,
+            this.y + oneBlockSize / 2
+        );
+        canvasContext.rotate((this.direction * 90 * Math.PI) / 180);
+        canvasContext.translate(
+            -this.x - oneBlockSize / 2,
+            -this.y - oneBlockSize / 2
+        );
+        canvasContext.drawImage(
+            mazegluttonFrames,
+            (this.currentFrame - 1) * oneBlockSize,
+            0,
+            oneBlockSize,
+            oneBlockSize,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+        canvasContext.restore();
+    }
 }
